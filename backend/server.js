cd21d1de4057e36d57c364015f68bee06de92387
srsohan125 <sohan.patel@swiftrut.com>
2024-10-24 23:56:41 +0530

    remove package.json backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const mongoose = require('./database/db.js'); 
-const userRouter = require('./routes/user.routes.js'); 
-const bodyParser = require('body-parser'); 
 const dotenv = require('dotenv');
-
 dotenv.config();
 
+const express = require('express');
+const mongoose = require('./database/db.js');
+const userRouter = require('./routes/user.routes.js');
+const bodyParser = require('body-parser');
+
 const app = express();
-const port = process.env.PORT || 5001; 
+const port = process.env.PORT || 5000;  
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use('/users', userRouter);
+app.use('/users', userRouter);  
 
 app.get("/", (req, res) => {
+    res.send("Welcome...!!");
+});
 
-    res.send("Welcome...!!")
-
-})
-
-
-app.listen(5000, () => {
-    console.log("server is running on 5000");
-})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
