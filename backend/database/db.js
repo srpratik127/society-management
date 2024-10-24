6b284a9128a00d9e93445a66e8aaa351a4485e9b
srsohan125 <sohan.patel@swiftrut.com>
2024-10-24 07:10:44 +0530

    register page create

---
+++ backend/database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/society-management-system', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((e) => {
+    console.log("MongoDB not connected", e.message);
+});
