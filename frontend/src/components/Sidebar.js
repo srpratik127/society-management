5dd6813f78a788d739cb2b6a3398154486de14cd
srmeet105 <meet.padhra@swiftrut.com>
2024-10-25 21:13:44 +0530

    update sidebar js file

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -18,17 +18,16 @@ const Sidebar = () => {
       <div className="flex-1">
         <ul className="space-y-2">
           {sidebardata.map((sidebar) => (
-              <React.Fragment key={sidebar.id}>
-               
+            <React.Fragment key={sidebar.id}>
               <li
-                className="flex items-center p-4 py-4 hover:bg-gray-100 cursor-pointer hover:text-white hover:bg-gradient-to-r from-[#FE512E] to-[#F09619] rounded-xl"
+                className="relative flex items-center p-4 py-4 cursor-pointer rounded-xl hover:text-white hover:bg-gradient-to-r from-[#FE512E] to-[#F09619] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-1 hover:before:bg-[#FE512E]"
                 onClick={() => toggleDropdown(sidebar.id)}
               >
+           
                 <span className="pr-3">
                   <img src={sidebar.icon} alt={`${sidebar.title} icon`} className="w-6 h-6" />
                 </span>
                 <span className="font-bold">{sidebar.title}</span>
-              
               </li>
 
               {sidebar.dropdown && activeDropdown === sidebar.id && (
@@ -47,15 +46,16 @@ const Sidebar = () => {
           ))}
         </ul>
       </div>
-      <div className="p-4 border-t  border-gray-200">
-        <button className="w-full flex items-center text-[#E74C3C] justify-center py-2 rounded hover:bg-gradient-to-r from-[#FE512E] to-[#F09619] hover:text-white">
+
+      <div className="p-4 border-t left-0 border-gray-200">
+        <button className="w-full flex items-center text-[#E74C3C] justify-start px-4 py-2 rounded hover:bg-gradient-to-r from-[#D3D3D3] to-[#D3D3D3] ">
           <span className="mr-3 text-black">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
               viewBox="0 -960 960 960"
               width="24px"
-              className="fill-[#E74C3C] hover:fill-white"
+              className="fill-[#E74C3C]"
             >
               <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
             </svg>
