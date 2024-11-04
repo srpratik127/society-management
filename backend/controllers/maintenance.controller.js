d13ac4a3923ae3b590dd765f0565605d44bb0ad3
maulik119 <maulik.rasadiya@swiftrut.com>
2024-11-04 21:37:18 +0530

    Update maintenence  Controller

--- backend/controllers/maintenance.controller.js
+++ backend/controllers/maintenance.controller.js
@@ -1,9 +1,20 @@
 const Maintenance = require('../models/maintenance.model')
 
-const addMaintenance = (req, res) => {
+const addMaintenance = async (req, res) => {
     try {
-        const { anount, penaltyAmount, dueDate, penaltyDay, status } = req.body;
-    } catch (error) {}
+        const { anount, penaltyAmount, dueDate, penaltyDay, status } = req.body; 
+        const maintenence = new Maintenance({
+            anount,
+            penaltyAmount,
+            dueDate,
+            penaltyDay,
+            status
+        });
+        await maintenence.save();
+        res.status(200).json({ message: 'Maintenence created successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating Maintenence', error: error.message });
+    }
 }
 
 const getStatus = async (req, res) => {
@@ -21,7 +32,6 @@ const getStatus = async (req, res) => {
         res.status(404).json({ message: 'Get status controller error' })
     }
 }
-
 
 const getAllStatus = async (req, res) => {
     try {
