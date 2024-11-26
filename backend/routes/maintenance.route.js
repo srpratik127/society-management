04e83400793d2e8e97fe074b16f403481852367b
maulik119 <maulik.rasadiya@swiftrut.com>
2024-11-26 21:58:48 +0530

    create middleware to authenticate route

--- backend/routes/maintenance.route.js
+++ backend/routes/maintenance.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/maintenance.controller.js");
+const {roleCheck} = require("../middleware/jwt-middleware.js")
 
 router.post("/", controller.addMaintenance);
-router.get("/total-maintenance", controller.getTotalAmount);
+router.get("/total-maintenance", roleCheck(["admin","resident"]), controller.getTotalAmount);
 router.get("/:status", controller.getStatus);
 router.get("/", controller.getAllStatus);
 router.put("/:maintenanceId", controller.paymentForMaintenance);
