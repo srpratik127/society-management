252186b9a48b76870f067c5c3b6d9677412384f7
desai-pratik <pratikdesai2274@gmail.com>
2024-11-28 21:36:21 +0530

    some change in dashboard

--- frontend/src/pages/admin/Dashboard.js
+++ frontend/src/pages/admin/Dashboard.js
@@ -18,7 +18,8 @@ const Dashboard = () => {
       try {
         const [maintenanceRes, expenseRes] = await Promise.all([
           axios.get(
-            `${process.env.REACT_APP_BASE_URL}/v1/api/maintenance/total-maintenance`, {
+            `${process.env.REACT_APP_BASE_URL}/v1/api/maintenance/total-maintenance`,
+            {
               withCredentials: true,
             }
           ),
@@ -51,28 +52,28 @@ const Dashboard = () => {
     {
       id: "1",
       title: "Total Balance",
-      amount: `₹ ${totalMaintenance - totalExpense}`,
+      amount: `₹ ${totalMaintenance - totalExpense}` || "00",
       icon: "/assets/1-4.svg",
       color: "#F7CA8C",
     },
     {
       id: "2",
       title: "Total Income",
-      amount: `₹ ${totalMaintenance}`,
+      amount: `₹ ${totalMaintenance}` || "00",
       icon: "/assets/money-recive.svg",
       color: "#9CCB9E",
     },
     {
       id: "3",
       title: "Total Expense",
-      amount: `₹ ${totalExpense}`,
+      amount: `₹ ${totalExpense}` || "00",
       icon: "/assets/money-send.svg",
       color: "#C2CFF9",
     },
     {
       id: "4",
       title: "Total Unit",
-      amount: `0${totalUnit}`,
+      amount: `0${totalUnit}` || "00",
       icon: "/assets/building-4.svg",
       color: "#F59BE1",
     },
